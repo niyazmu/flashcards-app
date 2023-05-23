1dc98b8e975d4bc54c5d02450a47eac764b4341e
Niyaz <niyaz1712@outlook.com>
2023-05-23 14:29:07 +0100

    feat(header): enable user to set deck name

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,6 +4,17 @@ import Modal from "./Modal.jsx";
 
 function Header() {
   const [modal, setModal] = useState(false);
+  const [deck, setDeck] = useState({});
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setDeck((prevDeckData) => {
+      return {
+        ...prevDeckData,
+        [name]: value,
+      };
+    });
+  }
 
   return (
     <>
@@ -17,7 +28,18 @@ function Header() {
         isVisible={modal}
         close={() => setModal(false)}
       >
-        <form></form>
+        <form>
+          <label className="mb-2 block font-medium" htmlFor="name">
+            Name
+          </label>
+          <input
+            className="w-full rounded border p-2 focus:text-blue-500 focus:outline-blue-500"
+            type="text"
+            name="name"
+            value={deck.name}
+            onChange={handleChange}
+          />
+        </form>
       </Modal>
     </>
   );
