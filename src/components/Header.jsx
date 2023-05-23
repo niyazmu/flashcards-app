0be0c7d3432136f44bc8fd39ef95dc742b74d8db
Niyaz <niyaz1712@outlook.com>
2023-05-23 14:20:36 +0100

    feat(header): add modal to create deck

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,24 @@
+import React, { useState } from "react";
+
+import Modal from "./Modal.jsx";
+
 function Header() {
+  const [modal, setModal] = useState(false);
+
   return (
     <>
-      <header></header>
+      <header>
+        <button className="btn-primary" onClick={() => setModal(true)}>
+          Create deck
+        </button>
+      </header>
+      <Modal
+        heading="Create deck"
+        isVisible={modal}
+        close={() => setModal(false)}
+      >
+        <form></form>
+      </Modal>
     </>
   );
 }
