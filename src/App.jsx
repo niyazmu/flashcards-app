3530ef0fe7d96beb8e6ffc2268264084cb6e553c
Niyaz <niyaz1712@outlook.com>
2023-05-25 18:28:23 +0100

    refactor: move decks and cards fetch requests to App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,45 @@
+import supabase from "./supabaseClient";
+
+import { useState, useEffect } from "react";
+
 import Main from "./components/Main.jsx";
 
 function App() {
+  const [decks, setDecks] = useState([]);
+  const [cards, setCards] = useState([]);
+
+  useEffect(() => {
+    fetchDecks();
+    fetchCards();
+  }, []);
+
+  async function fetchDecks() {
+    try {
+      const { data, error } = await supabase.from("decks").select("*");
+      if (error) throw error;
+      if (data != null) {
+        setDecks(data);
+      }
+    } catch (error) {
+      alert(error.message);
+    }
+  }
+
+  async function fetchCards() {
+    try {
+      const { data, error } = await supabase.from("cards").select("*");
+      if (error) throw error;
+      if (data != null) {
+        setCards(data);
+      }
+    } catch (error) {
+      alert(error.message);
+    }
+  }
+
   return (
     <>
-      <Main />
+      <Main decks={decks} cards={cards} />
     </>
   );
 }
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,42 +1,6 @@
-import supabase from "../../supabaseClient";
-
-import { useState, useEffect } from "react";
-
 import Deck from "./Deck.jsx";
 
-function Main() {
-  const [decks, setDecks] = useState([]);
-  const [cards, setCards] = useState([]);
-
-  useEffect(() => {
-    fetchDecks();
-    fetchCards();
-  }, []);
-
-  async function fetchDecks() {
-    try {
-      const { data, error } = await supabase.from("decks").select("*");
-      if (error) throw error;
-      if (data != null) {
-        setDecks(data);
-      }
-    } catch (error) {
-      alert(error.message);
-    }
-  }
-
-  async function fetchCards() {
-    try {
-      const { data, error } = await supabase.from("cards").select("*");
-      if (error) throw error;
-      if (data != null) {
-        setCards(data);
-      }
-    } catch (error) {
-      alert(error.message);
-    }
-  }
-
+function Main({ decks, cards }) {
   function countCards(deck_id) {
     const filteredCards = cards.filter((card) => card.deck_id === deck_id);
     const numberOfCards = filteredCards.length;
