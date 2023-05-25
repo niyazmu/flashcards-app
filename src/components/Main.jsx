18382f9c073e6029e844c50d96c5f9f16f7976c0
Niyaz <niyaz1712@outlook.com>
2023-05-25 16:46:50 +0100

    feat(deck): display decks from database

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,12 +1,39 @@
 import supabase from "../../supabaseClient";
 
+import { useState, useEffect } from "react";
+
 import Deck from "./Deck.jsx";
 
 function Main() {
+  const [decks, setDecks] = useState([]);
+
+  useEffect(() => {
+    fetchDecks();
+  }, []);
+
+  async function fetchDecks() {
+    try {
+      const { data, error } = await supabase.from("decks").select("*");
+      if (error) throw error;
+      if (data != null) {
+        setDecks(data);
+      }
+    } catch (error) {
+      alert(error.message);
+    }
+  }
+
   return (
     <>
       <main>
-        <Deck name="Untitled" numberOfCards="0" colour="bg-red-300" />
+        {decks.map((deck) => (
+          <Deck
+            key={deck.deck_id}
+            name={deck.name}
+            numberOfCards="0"
+            colour={deck.colour}
+          />
+        ))}
       </main>
     </>
   );
