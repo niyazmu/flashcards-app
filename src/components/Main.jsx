739c8249b5c73b470f9762922de4816cf2cebd89
Niyaz <niyaz1712@outlook.com>
2023-05-25 17:33:16 +0100

    feat(deck): display number of cards

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -6,9 +6,11 @@ import Deck from "./Deck.jsx";
 
 function Main() {
   const [decks, setDecks] = useState([]);
+  const [cards, setCards] = useState([]);
 
   useEffect(() => {
     fetchDecks();
+    fetchCards();
   }, []);
 
   async function fetchDecks() {
@@ -23,6 +25,24 @@ function Main() {
     }
   }
 
+  async function fetchCards() {
+    try {
+      const { data, error } = await supabase.from("cards").select("*");
+      if (error) throw error;
+      if (data != null) {
+        setCards(data);
+      }
+    } catch (error) {
+      alert(error.message);
+    }
+  }
+
+  function countCards(deck_id) {
+    const filteredCards = cards.filter((card) => card.deck_id === deck_id);
+    const numberOfCards = filteredCards.length;
+    return numberOfCards;
+  }
+
   return (
     <>
       <main>
@@ -30,7 +50,7 @@ function Main() {
           <Deck
             key={deck.deck_id}
             name={deck.name}
-            numberOfCards="0"
+            numberOfCards={countCards(deck.deck_id)}
             colour={deck.colour}
           />
         ))}
