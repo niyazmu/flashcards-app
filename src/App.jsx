f2505afa2d030cd2f874f2bf7088d5c5a426538b
Niyaz <niyaz1712@outlook.com>
2023-06-12 20:18:27 +0100

    feat(app): set up react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,15 @@
-import supabase from "./supabaseClient";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import { useState, useEffect } from "react";
-
-import Header from "./components/Header.jsx";
-import Main from "./components/Main.jsx";
+import Home from "./components/Home.jsx";
 
 function App() {
-  const [decks, setDecks] = useState([]);
-  const [cards, setCards] = useState([]);
-
-  useEffect(() => {
-    fetchDecks();
-    fetchCards();
-  }, []);
-
-  async function fetchDecks() {
-    try {
-      const { data, error } = await supabase.from("decks").select("*");
-      if (error) throw error;
-      if (data != null) {
-        setDecks(data);
-      }
-    } catch (error) {
-      alert(error.message);
-    }
-  }
-
-  async function fetchCards() {
-    try {
-      const { data, error } = await supabase.from("cards").select("*");
-      if (error) throw error;
-      if (data != null) {
-        setCards(data);
-      }
-    } catch (error) {
-      alert(error.message);
-    }
-  }
-
   return (
     <>
-      <div className="container mx-auto">
-        <Header decks={decks} />
-        <Main decks={decks} cards={cards} />
-      </div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
---
+++ src/components/Home.jsx
@@ -0,0 +1,51 @@
+import supabase from "../supabaseClient";
+
+import { useState, useEffect } from "react";
+
+import Header from "./Header.jsx";
+import Main from "./Main.jsx";
+
+function Home() {
+  const [decks, setDecks] = useState([]);
+  const [cards, setCards] = useState([]);
+
+  useEffect(() => {
+    fetchDecks();
+    fetchCards();
+  }, []);
+
+  async function fetchDecks() {
+    try {
+      const { data, error } = await supabase.from("decks").select("*");
+      if (error) throw error;
+      if (data != null) {
+        setDecks(data);
+      }
+    } catch (error) {
+      alert(error.message);
+    }
+  }
+
+  async function fetchCards() {
+    try {
+      const { data, error } = await supabase.from("cards").select("*");
+      if (error) throw error;
+      if (data != null) {
+        setCards(data);
+      }
+    } catch (error) {
+      alert(error.message);
+    }
+  }
+
+  return (
+    <>
+      <div className="container mx-auto">
+        <Header decks={decks} />
+        <Main decks={decks} cards={cards} />
+      </div>
+    </>
+  );
+}
+
+export default Home;
