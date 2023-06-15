c22f044d97c8dd411a8631aee8834e932fb98a3f
Niyaz <niyaz1712@outlook.com>
2023-06-15 21:54:32 +0100

    feat(carousel): create carousel component

---
+++ src/components/Carousel.jsx
@@ -0,0 +1,5 @@
+function Carousel({ cards }) {
+  return <h1>This is a card carousel.</h1>;
+}
+
+export default Carousel;
--- src/components/DeckPage.jsx
+++ src/components/DeckPage.jsx
@@ -1,12 +1,12 @@
 import { useParams } from "react-router-dom";
 
+import Carousel from "./Carousel";
+
 function DeckPage({ cards }) {
   const { deck_id } = useParams();
-
   const deckCards = cards.filter((card) => card.deck_id === parseInt(deck_id));
 
-  console.log(deckCards);
-  return <h1>This is the deck page.</h1>;
+  return <Carousel cards={deckCards} />;
 }
 
 export default DeckPage;
