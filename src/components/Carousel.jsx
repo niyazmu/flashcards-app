05ac6d416ce9f0c4c4dedd24e4701bba698e3c09
Niyaz <niyaz1712@outlook.com>
2023-06-18 19:53:21 +0100

    feat(carousel): get the next card when either button is clicked

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -2,15 +2,42 @@ import { useState } from "react";
 
 function Carousel({ cards }) {
   const [flipped, setFlipped] = useState(false);
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  function markCorrect() {
+    setFlipped(false);
+    const lastIndex = cards.length;
+    const newIndex = currentIndex + 1;
+    if (newIndex === lastIndex) {
+      return; // stop incrementing
+    }
+    setCurrentIndex(newIndex);
+  }
+
+  function markIncorrect() {
+    setFlipped(false);
+    const lastIndex = cards.length;
+    const newIndex = currentIndex + 1;
+    if (newIndex === lastIndex) {
+      return; // stop incrementing
+    }
+    setCurrentIndex(newIndex);
+  }
 
   return (
     <>
-      <div className="">{flipped ? cards[0].front : cards[0].back}</div>
-      <button className="">Mark incorrect</button>
+      <div className="">
+        {flipped ? cards[currentIndex].front : cards[currentIndex].back}
+      </div>
+      <button className="" onClick={markIncorrect}>
+        Mark incorrect
+      </button>
       <button className="" onClick={() => setFlipped(!flipped)}>
         Flip card
       </button>
-      <button className="">Mark correct</button>
+      <button className="" onClick={markCorrect}>
+        Mark correct
+      </button>
     </>
   );
 }
