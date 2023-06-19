f88fb8684615d975f6e861894bce5f2a0095a63f
Niyaz <niyaz1712@outlook.com>
2023-06-19 01:11:01 +0100

    feat(deck): update styles

--- src/components/Deck.jsx
+++ src/components/Deck.jsx
@@ -1,8 +1,8 @@
 function Deck({ name, numberOfCards, colour }) {
   return (
     <div className={`${colour} rounded-xl p-8`}>
-      <h1 className="mb-16 text-xl font-semibold">{name}</h1>
-      <span className="opacity-90">{numberOfCards} cards</span>
+      <h1 className="mb-16 text-xl font-semibold text-white">{name}</h1>
+      <span className="text-white">{numberOfCards} cards</span>
     </div>
   );
 }
