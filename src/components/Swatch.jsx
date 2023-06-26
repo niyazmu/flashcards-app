9b8bc53d09f81bf3620dc8e7f0f1590d1c7de779
Niyaz <niyaz1712@outlook.com>
2023-06-26 15:59:36 +0100

    feat(swatch): update style when selected

--- src/components/Swatch.jsx
+++ src/components/Swatch.jsx
@@ -2,9 +2,7 @@ function Swatch({ colour, checked, handleChange }) {
   return (
     <label
       htmlFor={colour}
-      className={`${
-        checked ? `border-2 border-${colour}-600` : ""
-      } flex h-[3.15rem] w-[3.15rem] bg-${colour}-400 rounded-sm`}
+      className={`flex h-[3.15rem] w-[3.15rem] bg-${colour}-400 relative cursor-pointer rounded-sm`}
     >
       <input
         className="hidden"
@@ -15,6 +13,11 @@ function Swatch({ colour, checked, handleChange }) {
         checked={checked}
         onChange={handleChange}
       />
+      {checked && (
+        <span className="absolute inset-2 flex items-center justify-center">
+          <span className="h-4 w-4 rounded-full border-2 border-white bg-transparent"></span>
+        </span>
+      )}
     </label>
   );
 }
