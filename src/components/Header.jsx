9f927497b1acf7d3c2f9716c23fc9be5a1227c76
Niyaz <niyaz1712@outlook.com>
2023-06-27 16:19:21 +0100

    feat(header): add search bar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,8 +8,16 @@ function Header({ decks }) {
 
   return (
     <>
-      <header>
-        <button className="" onClick={() => setModal(true)}>
+      <header className="my-8 flex items-center gap-4">
+        <input
+          className="flex-grow rounded-full border p-5 pl-8 outline-blue-500"
+          type="text"
+          maxLength="32"
+        />
+        <button
+          className="ml-auto rounded-full bg-blue-500 px-12 py-5 text-white"
+          onClick={() => setModal(true)}
+        >
           Create flashcards
         </button>
       </header>
