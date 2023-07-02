9a5653ab15c0afb39b10336ae295f8bed025ba86
Niyaz <niyaz1712@outlook.com>
2023-07-02 18:32:28 +0100

    feat(homepage): rewrite heading

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -69,14 +69,13 @@ function HomePage() {
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 24 24"
-                stroke-width="1.5"
-                stroke="currentColor"
-                class="h-6 w-6"
+                fill="currentColor"
+                className="h-6 w-6"
               >
                 <path
-                  d="M12 4.5v15m7.5-7.5h-15"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
+                  fillRule="evenodd"
+                  d="M12 3.75a.75.75 0 01.75.75v6.75h6.75a.75.75 0 010 1.5h-6.75v6.75a.75.75 0 01-1.5 0v-6.75H4.5a.75.75 0 010-1.5h6.75V4.5a.75.75 0 01.75-.75z"
+                  clipRule="evenodd"
                 />
               </svg>
             </button>
@@ -127,11 +126,11 @@ function HomePage() {
         </main>
       </div>
       <Modal
-        heading="Create flashcards"
+        heading="Create a new deck"
         isVisible={modal}
         close={() => setModal(false)}
       >
-        <CreateForm modal={modal} decks={decks} />
+        <CreateForm modal={modal} setModal={setModal} decks={decks} />
       </Modal>
     </>
   );
